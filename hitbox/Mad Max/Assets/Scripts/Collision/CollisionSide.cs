95f9a36dc168e3f433ea17c3eaf6fc4bb6292636
Fer239 <fer239@live.com>
2018-09-24 01:09:50 +0200

    solo assets y projectsettings

---
+++ hitbox/Mad Max/Assets/Scripts/Collision/CollisionSide.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSide : MonoBehaviour {
+
+    bool CR_running;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Damage();
+       // StartCoroutine("Cooldown");
+
+
+
+    }
+   
+    
+
+    
+    //Esto para intentar usar tiempo para que no le pueda volver a dar, pero ademas hay que poner velocidad de choque y transformarla en daño, eso es facil
+
+  //  IEnumerator Cooldown()     //De momento vamos a usar un cooldown paracuando le de un golpe no pueda alejarse 1cm y darle otra vez, tenga que ir con velocidad, eso mas tarde
+  //  {
+  //      CR_running = true;
+  //      yield return new WaitForSeconds(10);
+  //      CR_running = false;
+  //
+  //  }
+    void Damage()
+    {
+      //  if (CR_running)
+      //  {
+      //      PlayerCollision.CurrentLifePoints = 0;
+      //  }
+        PlayerCollision.CurrentLifePoints = 10;
+    }
+    
+}
---
+++ hitbox/Mad Max/Assets/Scripts/Collision/PlayerCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour {
+
+    //public static int Damage = 10; //de momento 10 para probar
+    public static int InternalLifePoints = 100;  //puntos de vida interna de momento para probar, hay que traer vida de un script de vida y aplicarla aqui
+    public static int CurrentLifePoints;   //en el script de vida poner; PlayerCollision.Currentlifepoints
+
+	void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);     //Dejar esta linea para saber a que toca el collider
+
+        if (collisionInfo.collider.tag == "CarPart")       //if para que solo haga daño cuando
+        {
+            Debug.Log("Has dado a parte " + collisionInfo.collider.name);
+            InternalLifePoints -= CurrentLifePoints;
+            Debug.Log(InternalLifePoints + " InternalLifePoints");
+            Debug.Log(CurrentLifePoints + " CurrentLifePoints");
+            //PlayerLifePoints.GetComponent<Text>().text = "" + InternalLifePoints;
+        }
+    }
+     void Update()
+    {
+        
+    }
+}
